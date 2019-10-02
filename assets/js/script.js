58a41de0bb69d9ad30417d83513162c5d46b4fd6
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2019-10-02 11:54:08 -0700

    Added card flipping functionality to all cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,42 @@
 $(document).ready(initializeApp);
 
+var allCards = null;
+var firstCardClicked = null;
+var secondCardClicked = null;
+var firstCardBack = null;
+var secondCardBack = null;
+var matches = null;
+
 function initializeApp() {
   $('.card').on('click', handleCardClick);
+  allCards = $('.card');
 }
 
 function handleCardClick(event) {
-  var theCardElement = $(event.currentTarget.lastElementChild);
-  theCardElement.toggleClass('hidden');
+  var theCardBack = $(event.currentTarget.lastElementChild);
+  theCardBack.addClass('hidden');
+
+  if (firstCardClicked === null) {
+    firstCardClicked = theCardBack;
+    firstCardBack = $(event.currentTarget).find('.cardFront').css('background-image');
+  }
+  else if (firstCardClicked !== null) {
+    secondCardClicked = theCardBack;
+    secondCardBack = $(event.currentTarget).find('.cardFront').css('background-image');
+    if (firstCardBack === secondCardBack) {
+      console.log('Cards Match');
+      matches++
+      firstCardClicked = null;
+      secondCardClicked = null;
+    } else {
+      allCards.attr('disabled', 'true');
+      setTimeout(function() {
+        firstCardClicked.removeClass('hidden');
+        secondCardClicked.removeClass('hidden');
+        firstCardClicked = null;
+        secondCardClicked = null;
+        allCards.removeAttr('disabled');
+        }, 1500);
+    }
+  }
 }
